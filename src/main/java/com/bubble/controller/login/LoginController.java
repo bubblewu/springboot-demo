17b9d2ec669553a88107552c8296ce2417a1c1ce
wugang <752820344@qq.com>
2019-04-03 18:57:05 +0800

    [修改]修改登录

--- src/main/java/com/bubble/controller/login/LoginController.java
+++ src/main/java/com/bubble/controller/login/LoginController.java
@@ -5,15 +5,11 @@ import com.bubble.domain.entity.user.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.data.jpa.domain.Specification;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
 
 /**
  * 登录
@@ -34,31 +30,23 @@ public class LoginController {
 
     @RequestMapping(value = "/login")
     public String login(UserEntity user, HttpServletRequest request) {
-        //登录成功
-        boolean flag = true;
         String result = "登录成功";
         //根据用户名查询用户是否存在
-        UserEntity userEntity = userRepository.findOne(new Specification<UserEntity>() {
-            @Override
-            public Predicate toPredicate(Root<UserEntity> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
-                criteriaQuery.where(criteriaBuilder.equal(root.get("name"), user.getName()));
-                return null;
+        Optional<UserEntity> userOpt = userRepository.findOne((Specification<UserEntity>) (root, criteriaQuery, criteriaBuilder) -> {
+            criteriaQuery.where(criteriaBuilder.equal(root.get("name"), user.getName()));
+            return null;
+        });
+
+        if (userOpt.isPresent()) {
+            UserEntity userEntity = userOpt.get();
+            if (!userEntity.getPwd().equals(user.getPwd())) { // 密码错误
+                result = "用户密码不相符，登录失败";
+            } else {// 登录成功
+                //将用户写入session
+                request.getSession().setAttribute("_session_user", userEntity);
             }
-        }).orElse(null);
-        //用户不存在
-        if (userEntity == null) {
-            flag = false;
+        } else { // 用户不存在
             result = "用户不存在，登录失败";
-        }
-        //密码错误
-        else if (!userEntity.getPwd().equals(user.getPwd())) {
-            flag = false;
-            result = "用户密码不相符，登录失败";
-        }
-        //登录成功
-        if (flag) {
-            //将用户写入session
-            request.getSession().setAttribute("_session_user", userEntity);
         }
         return result;
     }
